30a0d6a1e0c825deecfb2e85e7565f94a50cf161
David Kreyman <dkreyman@gmail.com>
2020-11-12 21:27:26 -0700

    Update main.go

--- main.go
+++ main.go
@@ -21,10 +21,11 @@ func main() {
 			RmvClip(i)
 		}
 		fmt.Printf("Uploading Video: %s, Number: %d ", vidinfo.Vidinfos[i].Name, i+1)
-		//Some string concatenation for the command
+		//Some string concatenation for the following command
 		file := "--filename=" + pathMP4
 		name := "--title=" + vidinfo.Vidinfos[i].Name
 		//Executes cmd command that uploads video to youtube
+		//Instead of implementing our own oauth and youtube upload feature we call on the script in the youtube folder.
 		out, err := exec.Command("go", "run", "youtube/oauth2.go", "youtube/upload_video.go", "youtube/errors.go", file, name).Output()
 		if err != nil {
 			fmt.Printf("%s", err)
